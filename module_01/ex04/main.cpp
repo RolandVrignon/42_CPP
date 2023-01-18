764a726d42c7bb0c2f1872e07fa90830a5cbc416
RolandVrignon <roland.vrignon@gmail.com>
2023-01-18 12:01:24 +0100

    finish sed ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:38:51 by rvrignon          #+#    #+#             */
-/*   Updated: 2023/01/18 10:21:30 by rvrignon         ###   ########.fr       */
+/*   Updated: 2023/01/18 12:00:51 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,50 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+
+std::string replace(std::string contents, int found, std::string s1, std::string s2)
+{
+	std::string a_std_string;
+	std::string b_std_string;
+	std::size_t length;
+	size_t		foundt = found;
+	
+	// Si l'occurence est trouve apres le debut alors on initialize le debut sous a_std_string
+	if (foundt > 0)
+	{
+		char a_char_array[found + 1];
+		length = contents.copy(a_char_array, found, 0);
+		a_char_array[length] = '\0';
+		a_std_string = a_char_array;
+	}
+	// Si l'occurence n'est pas la fin du contenu du texte, alors on initialize le texte apres l'occurence sous b_std_string
+	if (foundt < contents.length() - s1.length())
+	{
+		char b_char_array[contents.length() - found - s1.length()];
+		length = contents.copy(b_char_array, contents.length() - found - s1.length(), found + s1.length());
+		b_char_array[length] = '\0';
+		b_std_string = b_char_array;
+	}
+	
+	// Occurence trouver au debut, on concatene s2 et le reste
+	if (foundt == 0)
+	{
+		a_std_string = s2;
+		a_std_string.append(b_std_string);
+	}
+	// Occurence trouver a la fin, on concatene le debut et s2
+	else if (foundt == contents.length() - s1.length())
+	{
+		b_std_string = s2;
+		a_std_string.append(b_std_string);
+	}
+	// Occurence trouver au milieu, on concatene le debut, s2 et la fin
+	else
+	{
+		a_std_string.append(s2).append(b_std_string);
+	}
+	return (a_std_string);
+}
 
 int main(int ac, char **av)
 {
@@ -23,33 +67,26 @@ int main(int ac, char **av)
 		return (0);
 	}
 
-	std::string filein = av[1];
+	// Read file 
 	std::ifstream ifs(av[1]);
 	std::stringstream buffer;
 	buffer << ifs.rdbuf();
 	std::string contents(buffer.str());
-	std::cout << "Numbers of caracters of " << contents << " : " << contents.length() << std::endl;
-	
-	std::cout << "Parcourons le texte caractere par caractere" << std::endl;
-	std::size_t i = 0;	
-	while (i < contents.length())
-	{
-		std::cout << contents[i];
-		i++;
-	}
-
-	std::cout << std::endl << "Verifions les parametres :" << std::endl;
 	std::string s1 = av[2];
 	std::string s2 = av[3];
+	std::string filein = av[1];
 	std::string fileout = filein.append(".replace");
-	std::cout << "s1 : " << s1 << std::endl << "s2 : " << s2 << std::endl << "fileout : " << fileout << std::endl;
-	
+	std::size_t found = contents.find(av[2], 0, 2);
+	while (found != std::string::npos)
+	{
+		contents = replace(contents, found, s1, s2);
+		found = contents.find(av[2], found + 1, s1.length());
+	}
 	ifs.close();
 
-	//-----------------------
-
-	std::ofstream ofs("replace.txt");
-	ofs << "Yo man" << std::endl;
+	// Write in output file the new content
+	std::ofstream ofs(fileout.c_str());
+	ofs << contents << std::endl;
 	ofs.close();
 	
 	return (0);
