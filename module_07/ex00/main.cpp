a347ce37c393ded1b8bad6534bd4c70b19c2560d
RolandVrignon <roland.vrignon@gmail.com>
2023-02-02 13:34:58 +0100

    minor changes

--- module_07/ex00/main.cpp
+++ module_07/ex00/main.cpp
@@ -6,19 +6,24 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:32:06 by rvrignon          #+#    #+#             */
-/*   Updated: 2023/01/26 16:49:18 by rvrignon         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:28:05 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 int main () {
+    
+    // With INT
+    std::cout << "##### Testing with ints #####" << std::endl;
     int a = 2;
     int b = 3;
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << std::endl;
+    std::cout << "##### Testing with strings #####" << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
     ::swap(c, d);
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 18:16:04 by rvrignon          #+#    #+#             */
-/*   Updated: 2023/01/26 19:24:36 by rvrignon         ###   ########.fr       */
+/*   Updated: 2023/02/02 13:34:00 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "Array.cpp"
 
 int main (void) {
+    std::cout << "##### Constructors ######" << std::endl << std::endl;
     Array<int> int_empty;
     Array<int> int_filled(5);
     
@@ -27,11 +28,14 @@ int main (void) {
     Array<int> int_overload;
     int_overload = int_filled;
     
+    
+    std::cout << std::endl << "##### Get size of differents arrays ######" << std::endl << std::endl;
     std::cout << int_empty.getSize() << std::endl;
     std::cout << int_filled.getSize() << std::endl;
     std::cout << int_assigned.getSize() << std::endl;
     std::cout << int_overload.getSize() << std::endl;
-    
+
+    std::cout << std::endl << "##### Get Values of differents index ######" << std::endl << std::endl;
     try {
         std::cout << int_filled[0] << std::endl;
         std::cout << int_assigned[1] << std::endl;
@@ -40,7 +44,8 @@ int main (void) {
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-
+    
+    std::cout << std::endl << "##### Destructors ######" << std::endl << std::endl;
 
 
     return (0);
